3f77d95d621aa7a140757cdc1ba2f211e8e58130
fabioFDHelp <contatofdhelp@gmail.com>
2024-06-04 15:17:57 -0300

    Versao final do codigo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-    // TODO: Conhecer e importar a classe Scanner
-    
-    // Exibir as mensagens para o nosso usuário
+       public static void main(String[] args) throws Exception {
+   
+    //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
 
     // Obter pela classe Scanner os valores digitados no Terminal
 
+    // Exibir as mensagens para o nosso usuário
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
     // Exibir a mensagem da conta ao final
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
-    numeroConta int
-    agenciaConta String
-    nomeCliente String
-    saldoConta float
-    
+        // Fechar o scanner
+        scanner.close();
     }
-    public class ContaTerminal {
-    
-        
-    }
-    
-
 }
+
