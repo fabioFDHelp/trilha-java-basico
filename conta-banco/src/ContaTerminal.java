6f9c8b9fd6de6c8fb9cb81a5c0e825cbfb198cbe
fabio <contatofdhelp@gmail.com>
2024-06-03 21:56:05 -0300

    Primeiras demandas

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // TODO: Conhecer e importar a classe Scanner
+    
+    // Exibir as mensagens para o nosso usuário
+
+    // Obter pela classe Scanner os valores digitados no Terminal
+
+    // Exibir a mensagem da conta ao final
+
+    numeroConta int
+    agenciaConta String
+    nomeCliente String
+    saldoConta float
+    
     }
+    public class ContaTerminal {
+    
+        
+    }
+    
+
 }
